cd5268e06610c308236363da8f6f878065e74b2b
abhishekd-tcs <abhishek.dangeti@tcs.com>
2025-04-16 17:59:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ elif theme_type == "Thematic":
 #         metadata.append(artwork_path+'Photograph/'+arts[i]+'/metadata.txt')
 st.title("Composite Reflections")
 st.subheader("Composite Reflections is a method of artwork creation wherein we reimagine an artwork as composed from other constituent artworks. The constituents bear an intimate relation with the main subject.")
-st.markdown("### [← Different themes can be seen by expanding the sidebar on the left. (If the sidebar is not visible, please click the arrow in the top left corner.]")
+st.markdown("#### [← Different themes can be seen by expanding the sidebar on the left. (If the sidebar is not visible, please click the arrow in the top left corner.]")
 st.header(theme_type+" based Composite Reflections")
 
 
